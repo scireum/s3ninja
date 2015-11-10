3ebbb94bd24e4c7373eff2571e06894c6e0dd41e
aha <aha@scireum.de>
2015-11-10 12:05:27 +0100

    Fixed tests

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -11,7 +11,7 @@ import com.googlecode.junittoolbox.WildcardPatternSuite;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import sirius.kernel.BaseSpecification;
+import sirius.kernel.TestHelper;
 
 @RunWith(WildcardPatternSuite.class)
 @SuiteClasses({"**/*Test.class", "**/*Spec.class"})
@@ -19,11 +19,12 @@ public class TestSuite {
 
     @BeforeClass
     public static void setUp() {
-        BaseSpecification.setupSiriusTestEnvironment();
+        TestHelper.setUp();
     }
 
     @AfterClass
     public static void tearDown() {
-        BaseSpecification.cleanupSiriusTestEnvironment();
+        TestHelper.tearDown();
     }
+
 }

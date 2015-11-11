7ae92c15fa4e3f847db44ae16171e2a7839111bd
Duane Johnson <duane@instructure.com>
2015-11-11 12:57:28 -0700

    Missing semi-colon

--- src/main/java/ninja/S3Controller.java
+++ src/main/java/ninja/S3Controller.java
@@ -404,7 +404,7 @@ public class S3Controller implements Controller {
 
         object.storeProperties(properties);
         Response response = ctx.respondWith();
-        response.addHeader(HttpHeaders.Names.ETAG, etag(hash)).status(HttpResponseStatus.OK)
+        response.addHeader(HttpHeaders.Names.ETAG, etag(hash)).status(HttpResponseStatus.OK);
         response.addHeader(HttpHeaders.Names.ACCESS_CONTROL_EXPOSE_HEADERS, "ETag");
         signalObjectSuccess(ctx);
     }

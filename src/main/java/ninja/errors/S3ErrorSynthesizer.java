77bc58fa25a69cfb06cb170e800735e6b7febe7e
Jakob Vogel <post@jakobvogel.net>
2019-12-27 13:06:05 +0100

    Adds synthesizer for S3-conformant error messages 🚨
    #123

---
+++ src/main/java/ninja/errors/S3ErrorSynthesizer.java
@@ -0,0 +1,59 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package ninja.errors;
+
+import sirius.kernel.commons.Strings;
+import sirius.kernel.di.std.Register;
+import sirius.kernel.xml.XMLStructuredOutput;
+import sirius.web.http.WebContext;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Synthesizes <em>S3</em> <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html">error
+ * responses</a>.
+ */
+@Register(classes = S3ErrorSynthesizer.class)
+public class S3ErrorSynthesizer {
+
+    /**
+     * Synthesizes an error response.
+     *
+     * @param ctx the request to process.
+     * @param bucket the requested bucket, potentially <b>null</b>.
+     * @param key the requested object's key, potentially <b>null</b>.
+     * @param code the error code to send.
+     * @param message a human-readable description of the error.
+     */
+    public void synthesiseError(@Nonnull WebContext ctx,
+                                @Nullable String bucket,
+                                @Nullable String key,
+                                @Nonnull S3ErrorCode code,
+                                @Nullable String message) {
+        XMLStructuredOutput
+                xml = new XMLStructuredOutput(ctx.respondWith()
+                                                 .outputStream(code.getHttpStatusCode(), "text/xml"));
+
+        String resource = null;
+        if (Strings.isFilled(bucket)) {
+            resource = "/" + bucket;
+            if (Strings.isFilled(key)) {
+                resource += "/" + key;
+            }
+        }
+
+        xml.beginOutput("Error");
+        xml.property("Code", code.toString());
+        xml.propertyIfFilled("Message", message);
+        xml.propertyIfFilled("Resource", resource);
+        // xml.property("RequestId", XXX); // a real error includes this request id that would need to be generated here
+        xml.endOutput();
+    }
+}

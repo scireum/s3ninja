a333b2f4497ec9b6d24c52c9fb5980951f5bc884
Jakob Vogel <jvo@scireum.de>
2025-06-30 22:33:26 +0200

    Restores former Groovy test suite 🧪

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,20 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+import com.googlecode.junittoolbox.SuiteClasses;
+import org.junit.runner.RunWith;
+import sirius.kernel.ScenarioSuite;
+
+/**
+ * Executes all tests.
+ */
+@RunWith(ScenarioSuite.class)
+@SuiteClasses({"**/*Test.class", "**/*Spec.class"})
+public class TestSuite {
+
+}

7f885f44488669e21e6bf740c3c228ed5716d1cd
Jakob Vogel <jvo@scireum.de>
2025-07-01 22:41:58 +0200

    Revert "Removes empty test 🗑️"
    This reverts commit 942a6a0f1a00f00e0cf797173dbf670276cd9da7 as there seems to be a compulsory groovy stage in sirius-parent

---
+++ src/test/java/EmptySpec.groovy
@@ -0,0 +1,12 @@
+import sirius.kernel.BaseSpecification
+
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+class EmptySpec extends BaseSpecification {
+}
